e68f49d72bb9ab6c769215c99bdf4c1bc05d5bcd
Tajmat-Uzair <uzairtajmat@outlook.com>
2024-04-23 16:16:30 +0530

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./App.css";
-
+import video from "./assets/Test3.mp4";
+import img from "./assets/Img1.jpg";
 function App() {
   return (
     <>
       <div class="hero-content">
-        <video autoplay muted loop id="video-background">
-          <source src="./assets/Test.mp4" type="video/mp4"></source>
-        </video>
-        <div class="text">
-          <h1>
-            Shaadi Karni Hai ...
-            <br />
-            Aajao Kara Denge
-          </h1>
-        </div>
-        <img src="hero-image.jpg" alt="Hero Image"></img>
+        <video
+          autoPlay
+          muted
+          id="video-background"
+          src={video}
+          height="350px"
+        ></video>
+
+        <h1>
+          Shaadi Karni Hai ...
+          <br />
+          Aajao Kara Denge
+        </h1>
       </div>
+      <img src={img} alt="Hero Image" className="img"></img>
     </>
   );
 }
